bfaab74fcf5a66d5f2ccd192ba7775e0bdd4369c
cristiano motta <cristiano.motta@hotmail.it>
2017-11-18 16:29:08 +0100

    Add project files.

---
+++ SerilogWeb/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+
+namespace SerilogWeb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            
+            // Serilog configuration
+            // https://stackoverflow.com/questions/40880261/configuring-serilog-rollingfile-with-appsettings-json
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            Log.Debug("Hello, world!");
+
+            BuildWebHost(args).Run(); 
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseSerilog()
+                .Build();
+    }
+}
